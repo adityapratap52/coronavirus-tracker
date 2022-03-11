9c3b456cbdc3a11b13e63dc2adf1ad5648df89be
adityapratap52 <adityapratap1490@gmail.com>
2022-03-11 17:51:22 +0530

    This application track corona patient. It tracks patient to particular Github(CSSEGISendData)

---
+++ src/main/java/com/coronavirus/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.coronavirus.controller;
+
+import com.coronavirus.model.LocationStats;
+import com.coronavirus.service.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService service;
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<LocationStats> allStats = service.getAllStats();
+
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffPrevDay()).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+
+}
